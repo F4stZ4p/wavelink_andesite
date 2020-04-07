45ab8e1512af47a01521260984109e356c0e9ab4
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2020-04-07 18:21:36 +0300

    Update stats.py

--- wavelink/stats.py
+++ wavelink/stats.py
@@ -61,9 +61,13 @@ class Stats:
         if not node.andesite:
 
             frame_stats = data.get('frameStats', {})
-            
+
             self.frames_sent = frame_stats.get('sent', -1)
             self.frames_nulled = frame_stats.get('nulled', -1)
             self.frames_deficit = frame_stats.get('deficit', -1)
+        
+        else:
+
+            self.frames_sent, self.frames_nulled, self.frames_deficit = -1
 
         self.penalty = Penalty(self)
